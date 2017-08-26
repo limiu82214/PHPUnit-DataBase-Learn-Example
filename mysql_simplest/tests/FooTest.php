3e260e527c42bae613194e3e3832082c236ae5a6
limiu82214 <limiu82214@gmail.com>
2017-08-26 21:06:02 +0800

    共享的資料庫連結，並且分離出登入MySQL資料

--- mysql_simplest/tests/FooTest.php
+++ mysql_simplest/tests/FooTest.php
@@ -2,71 +2,52 @@
 require_once dirname(__FILE__) . "/../src/Foo.php";
 
 class FooTest extends \PHPUnit_Extensions_Database_TestCase {
-    //一定要實做的兩個抽像 getConnection(), getDataSet()
 
-    // PHPUnit_Extensions_Database_TestCase 的抽像
-    // 所以一定要實做
-    // 建立暫時資料庫的連接
+    // 共享連結資料庫的pdo和connection，防止無意義的重覆連結資料庫。
+    static private $pdo = null;
+    private $connection = null;
+
+    // 把連結資料庫的參數從function分離出來
+    private static $db_dbname   = "test_db";
+    private static $db_dsn_type = "mysql";
+    private static $db_dsn_host = "localhost";
+    private static $db_account  = "nimo";
+    private static $db_password = "nimo";
+
     public function getConnection() {
-
-        // 因為
-        //      PHPUnit_Extensions_Database_defaultTester::_construct() 必需實作
-        //      介面 -> PHPUnit_Extensions_Database_DB_IDatabaseConnection
-        // 所以
-        //      使用 createDefaultDBConnection()。
-        //
-        // 因為 (用來建立與臨時的資料庫)
-        //      createDefaultDBConnection() 第一個參數必需是 PDO 的實作
-        //      第二個參數是資料表名稱
-        // 所以
-        //      先建立變數pdo
-        //      這裡必需要有 帳密為nimo 的mysql用戶，並且已存在test_db資料庫
-        //      並且裡面已有資料表 test_table 格式為：
-        //          sn      int(11)         AUTO_INCREMENT
-        //          name    varchar(50)
-        //          age     int(11)
-        //      (你可以自己修改)
-        $dbname = "test_db";
-        $dsn = "mysql:host=localhost;dbname={$dbname}";
-        $account = "nimo";
-        $password = "nimo";
-        $pdo = new PDO($dsn, $account, $password);
-        return $this->createDefaultDBConnection($pdo, $dbname);
+        // 這裡參考了官方手冊的建立連線方式，減少重覆連結的次數
+        if ($this->connection === null) {
+            if (self::$pdo == null) {
+                $dsn = self::$db_dsn_type
+                    .":host="  . self::$db_dsn_host
+                    .";dbname=". self::$db_dbname
+                ;
+                $account   = self::$db_account;
+                $password  = self::$db_password;
+                self::$pdo = new PDO( $dsn, $account, $password);
+            }
+            $this->connection = $this->createDefaultDBConnection(
+                self::$pdo,
+                self::$db_dbname
+            );
+        }
+        return $this->connection;
     }
 
-    // PHPUnit_Extensions_Database_TestCase 的抽像
-    // 所以一定要實做
-    // 建立暫時資料庫的初始值
     public function getDataSet() {
-        //  must implement interface
-        // 因為
-        //      PHPUnit_Extensions_Database_AbstractTester::setDataSet()必需實作
-        //      介面 -> PHPUnit_Extensions_Database_DataSet_IDataSet
-        // 所以
-        //      使用 createFlatXMLDataSet()。
-        return $this->createXMLDataSet(__DIR__ . '/_files/testSeedData.xml'); // Seed 表示種子(初始)
-
+        return $this->createXMLDataSet(__DIR__ . '/_files/testSeedData.xml');
     }
 
-    // 至少要有一個測試
     public function testSimple() {
-        // 為了讓範例看起來很簡單，這邊直接復製了一遍上面的連接方式
-        $dbname = "test_db";
-        $dsn = "mysql:host=localhost;dbname={$dbname}";
-        $account = "nimo";
-        $password = "nimo";
-        $pdo = new PDO($dsn, $account, $password);
-        $db = $this->createDefaultDBConnection($pdo, $dbname);
+        // 直接使用已經建好的pdo
+        $conn = $this->connection;
+        $pdo = self::$pdo;
 
         $foo = new Foo($pdo);
 
-        // 資料庫初始(見testSeedData.xml)有兩筆資料，這邊插入一筆，所有共有三筆
         $foo->insert(array('name'=>'dong', 'age'=>'23'));
 
-        // 用 createQueryTable() 取出資料庫現在的資料
-        $actual = $db->createQueryTable('test_table', 'SELECT * FROM test_table');
-
-        // 拿出先準備好的資料表 ( 他應該要與加完一筆資料後的資料表一樣 )
+        $actual   = $conn->createQueryTable('test_table', 'SELECT * FROM test_table');
         $data_set = $this->createXMLDataSet(__DIR__ . '/_files/testExpectedData.xml');
         $expected = $data_set->getTable('test_table');
 
